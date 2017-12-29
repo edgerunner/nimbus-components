0180e927e26a18b597408ef9d7735e5d4553acd6
Mert Torun <mert@merttorun.com>
2017-12-30 00:35:16 +0300

    Tidy up regex object and add validation helper

--- src/regex.js
+++ src/regex.js
@@ -1,7 +1,16 @@
-const email = /^\s*[\w-+.]+?@[\w-]+?(?:\.[\w-]+?)*?\s*$/;
+const regex = {
+    email: /^\s*[\w-+.]+?@[\w-]+?(?:\.[\w-]+?)*?\s*$/,
+    fullname: /^(?!(?:.*\d))\s*(\S+)(?:\s+\S+)+\s*$/,
+    password: /^(?=(?:.*[a-z]))(?=(?:.*[A-Z]))(?=(?:.*\d))\S{6,40}$/,
+    blank: /^\s*$/,
+    validatedValue: (v, r) => {
+        const s = new String(v);
+        s.validator = r;
+        Object.defineProperty(s, 'blank', { get: function() { return /^\s*$/.test(this); } } );
+        Object.defineProperty(s, 'valid', { get: function() { return this.validator.test(this); } } );
+        Object.defineProperty(s, 'error', { get: function() { return !this.blank && !this.valid; } } );
+        return s;
+    }
+}
 
-const fullname = /^(?!(?:.*\d))\s*(\S+)(?:\s+\S+)+\s*$/;
-
-const password = /^(?=(?:.*[a-z]))(?=(?:.*[A-Z]))(?=(?:.*\d))\S{6,40}$/;
-
-export default {email, fullname, password}
+export default regex;
